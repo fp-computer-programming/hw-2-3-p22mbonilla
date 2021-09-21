94fd778a89574aefdf74c9c2b2949324fab0dbbb
p22mbonilla <p22mbonilla@fairfieldprep.org>
2021-09-21 09:43:34 -0400

    add file

--- hw2-3-#.py
+++ hw2-3-#.py
@@ -1,2 +1,6 @@
 # Author MB 09/20/2021
 
+print("PPPPP  Y   Y  TTTTT  H   H   OOOOO    N   N")
+print("P   P   Y Y     T    H   H   O   O    NN  N")
+print("PPPPP    Y      T    HHHHH   O   O    N N N")
+print("P        Y      T    H   H   O   O    N  NN")
