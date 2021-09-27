dda65b842bfabf267e0557067be02ebef43ac582
p22mbonilla <p22mbonilla@fairfieldprep.org>
2021-09-27 10:19:04 -0400

    add file

---
+++ hw2-3-3.py
@@ -0,0 +1,10 @@
+# Author MB 09/26/2021
+
+width = 13.9
+hei = 8.2
+
+area = width * hei
+peri = width ** 2 + hei ** 2
+
+print(area)
+print(peri)
---
+++ hw2-3-4.py
@@ -0,0 +1,11 @@
+# Author MB 09/26/2021
+
+ori_pop = 331832432
+days = 366
+pop_per_day = 13826
+
+total_pop = ori_pop + (days * pop_per_day)
+
+total_pop = str(total_pop)
+
+print(total_pop)
